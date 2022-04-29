dc310d3fdccfa14e8518866b004ca371f152bcf4
HameemH <hameemhossain2@gmail.com>
2022-04-29 17:05:54 +0600

    1st api and load data form mongo

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = "mongodb+srv://laptopInven1:ChOk4HIBc4weiEnK@cluster0.sroqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+   try{
+    await client.connect();
+    const collection = client.db("Inventory").collection("laptops");
+    console.log(collection);
+    app.get('/item', async (req, res) => {
+        const query = {};
+        const cursor = collection.find(query);
+        const items = await cursor.toArray();
+      
+        res.send(items);
+    });
+   }
+
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+ 
 
 app.get('/', (req, res) =>{
     res.send('Running My Node  Server');
