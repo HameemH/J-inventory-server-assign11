774763dd32803999fa3847b051be07cee337b71c
HameemH <hameemhossain2@gmail.com>
2022-05-07 14:50:36 +0600

    dot env added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://laptopInven1:ChOk4HIBc4weiEnK@cluster0.sroqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sroqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
    try{
@@ -18,6 +19,7 @@ async function run(){
     
     app.get('/item', async (req, res) => {
         const query = {};
+        console.log('db connected');
         const cursor = collection.find(query);
         const items = await cursor.toArray();
         res.send(items);
